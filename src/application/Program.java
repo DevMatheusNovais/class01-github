2e99ceef299bd6d76aa3396f9201c43f246eb3f3
DevMatheusNovais <matheus.novais@icloud.com>
2020-03-10 17:45:11 -0300

    Projeto Criado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+	
+		int N = sc.nextInt();
+		
+		for (int i=1; i<=10; i++) {
+			int produto = i * N;
+			System.out.println(i + " x " + N + " = " + produto);
+		}
+		
+		sc.close();
 	}
-
 }
